d03479730ef9c1c828cc6b5f2a7fe27e71cdc00e
Willi24am <80512313+Willi24am@users.noreply.github.com>
2021-04-17 02:13:31 +0700

    Add files via upload

---
+++ I0320110_Soal1_Tugas7.py
@@ -0,0 +1,15 @@
+header = "Mari Berkenalan!"
+x = header.center(20,'@')
+print(x)
+
+str = "william adalah orang yang sangat aktif"
+y = str.capitalize()
+print(y)
+print("apakah william malas?", str.find('malas'))
+print("apakah william aktif?", str.endswith('aktif'))
+
+z = str.replace("malas", "aktif")
+print(z)
+teks = "saya ingin cepat lulus kuliah"
+a = teks.upper()
+print(a)
---
+++ I0320110_Soal2_Tugas7.py
@@ -0,0 +1,31 @@
+# Nomor 1
+import math
+print("1. John berlari ke arah timur sejauh 7 meter. \nKemudian belok ke arah selatan sejauh 5 meter. \nBerapakah perpindahan yang dialami john?")
+a = 7
+b = 5
+c = a ** 2 + b ** 2
+print("perpindahan john= ", math.sqrt(c))
+
+# Nomor 2
+print("\n2. Nilai UAS telah dibagikan, berikut adalah nilai tertinggi dan terendah yang William capai.")
+math = 86
+biologi = 97
+kimia = 83
+fisika = 99
+print("nilai tertinggi william = ", max(math,biologi,kimia,fisika))
+print("nilai terendah william = ", min(math,biologi,kimia,fisika))
+
+# Nomor 3
+w = -21
+x = -17
+y = -8
+z = -51
+print("\n3. Berikut merupakan pemutlakan keempat variabel di bawah")
+print("w= ", w)
+print("x= ", x)
+print("y= ", y)
+print("z= ", z)
+print("\n|w|= ", abs(w))
+print("|x|= ", abs(x))
+print("|y|= ", abs(y))
+print("|z|= ", abs(z))
